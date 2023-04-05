d62ce905f3c17b1857a515eca023c076baeb01d0
tejachalmala <vk21abz@herts.ac.uk>
2023-04-05 18:07:57 +0100

    cosmetic updates

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from helper_functions import load_dataframe
 from plotting_functions import plot_correlation, plot_scatter
 from config import data_path, indicator_metadata_path, country_metadata_path
 
-# from plotting functions import pie_chart, line_chart, bar_chart
-
 # Loading metadata related to indicators
 indicators_metadata = load_dataframe(indicator_metadata_path)
 indicators_metadata = indicators_metadata.drop(columns=["Unnamed: 4"])
 print("============ Indicators List =============")
-print(indicators_metadata.INDICATOR_NAME)
+print(indicators_metadata.INDICATOR_NAME.head(5))
 
 # Loading metadata related to indicators
 country_metadata = load_dataframe(country_metadata_path)
@@ -25,7 +23,7 @@ for region in distinct_regions:
     print(
         country_metadata[country_metadata["IncomeGroup"] == region][
             ["TableName", "Region"]
-        ]
+        ].head()
     )
 
 # Loading main data with transformation
@@ -33,36 +31,47 @@ year_df, country_df = load_dataframe(data_path, transform=True)
 
 # Picking few countries based on different income ranges
 selected_countries = ["Australia", "Thailand", "India", "Yemen, Rep."]
-indicators_of_interest = [
-    "Urban population (% of total population)",
-    "Population in urban agglomerations of more than 1 million (% of total population)",
-    "Population living in areas where elevation is below 5 meters (% of total population)",
+indicators_codes = [
+    "SP.URB.TOTL.IN.ZS",
+    "EN.URB.MCTY.TL.ZS",
+     "EN.POP.EL5M.UR.ZS"
 ]
+indicators_of_interest = [
+    list(
+        indicators_metadata[
+        indicators_metadata["INDICATOR_CODE"] == x]
+        ["INDICATOR_NAME"].values
+        )[0]
+        for x in indicators_codes]
 
 # Exploring statistical properties for indicators
 for indicator in indicators_of_interest:
-    print(f"================={indicator}===================")
-    print(country_df[selected_countries + ["World"]].loc[indicator].describe())
+    print(f"=================Description of {indicator}===================")
+    print(country_df[selected_countries + ["World"]].loc[indicator]
+    .describe())
 
 # Creating dataframes wrt skew and kurtosis for paired indicators
-skew_df = pd.DataFrame(columns=indicators_of_interest, index=selected_countries)
-kurtosis_df = pd.DataFrame(columns=indicators_of_interest, index=selected_countries)
+skew_df = pd.DataFrame(
+    columns=indicators_of_interest,
+    index=selected_countries)
+     
+kurtosis_df = pd.DataFrame(
+    columns=indicators_of_interest,
+    index=selected_countries)
+
 for indicator in indicators_of_interest:
     for country in selected_countries + ["World"]:
-        skew_df[indicator][country] = skew(country_df[country].loc[indicator].values)
-        kurtosis_df[indicator][country] = kurtosis(
-            country_df[country].loc[indicator].values
-        )
+        data = country_df[country].loc[indicator].values
+        skew_df[indicator][country] = skew(data)
+        kurtosis_df[indicator][country] = kurtosis(data)
 
-print("SKEW", skew_df)
-print("Kurtosis", kurtosis_df)
+print("======================== SKEW ======================", skew_df)
+print("======================== Kurtosis ======================", kurtosis_df)
 
 # Exploring correlations between indicators
 countries = ["Japan", "Sudan"]
-indicators = [
-    "Urban population (% of total population)",
-    "Population in urban agglomerations of more than 1 million (% of total population)",
-]
+indicators = indicators_of_interest[:2]
+print(indicators)
 plot_correlation(
     year_df,
     countries,
@@ -75,22 +84,23 @@ plot_correlation(
 )
 
 
-# Exploring correlations between indicators
 countries = ["China", "Bangladesh"]
-indicators = ["Urban population (% of total population)", "CO2 emissions (kt)"]
+indicators = [
+    "Urban population (% of total population)",
+    "CO2 emissions (kt)"
+     ]
 plot_scatter(
     year_df,
     indicators,
     countries,
     [
-        "population vs CO2",
-        "population growth",
+        "urban population vs CO2 emission",
+        "urban population",
         "CO2 emissions"
     ],
 )
 
 
-# Exploring correlations between indicators
 countries = ["Malta", "Yemen, Rep."]
 indicators = ["Population growth (annual %)", "Arable land (% of land area)"]
 plot_correlation(
